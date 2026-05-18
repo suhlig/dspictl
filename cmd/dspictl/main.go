3ecaf10b60ef22a889b9497b97e1627baf354d73
Steffen Uhlig <steffen@familie-uhlig.net>
2026-05-18 22:42:46 +0200

    Refactor command

--- cmd/dspictl/main.go
+++ cmd/dspictl/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/suhlig/dspi"
 )
 
 func main() {
@@ -36,39 +35,4 @@ func newRootCmd() *cobra.Command {
 	rootCmd.AddCommand(newGetMasterVolumeCmd())
 
 	return rootCmd
-}
-
-func newGetMasterVolumeCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "get-master-volume",
-		Short: "Print the current master volume of connected DSPi devices",
-		RunE:  runGetMasterVolume,
-	}
-}
-
-func runGetMasterVolume(cmd *cobra.Command, args []string) error {
-	devices, err := dspi.OpenAll()
-
-	if err != nil {
-		return fmt.Errorf("opening DSPi devices: %w", err)
-	}
-
-	defer func() {
-		for _, d := range devices {
-			d.Close()
-		}
-	}()
-
-	for _, d := range devices {
-		gain, err := d.GetMasterVolume()
-
-		if err != nil {
-			slog.Error("getting master volume failed", "serial", d.Serial(), "error", err)
-			continue
-		}
-
-		fmt.Printf("%s: %s\n", d.Serial(), gain)
-	}
-
-	return nil
 }
